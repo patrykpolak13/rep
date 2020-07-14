fb2a38c0112559df144f262bbb5fa05565401508
patrykpolak13 <68032378+patrykpolak13@users.noreply.github.com>
2020-07-14 16:38:40 +0200

    Delete SqlPersonRepository.java

--- demo1234-master/src/main/java/com/example/demo1234/model/SqlPersonRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo1234.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface SqlPersonRepository extends PersonRepository, JpaRepository<Person,Integer> {
-
-}
